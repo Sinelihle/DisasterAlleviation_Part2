15247ca35b339867b20bad0d6d68f0674f2ebd15
Sinelihle Ngqaza <ST10127517@rcconnect.edu.za>
2022-10-13 13:55:21 +0200

    updated Donate page c#, changed the json lauch setting to not allow unauthenticated users, created interface for monetary allocations page

--- DisasterAlleviation/Pages/Allocate.cshtml.cs
+++ DisasterAlleviation/Pages/Allocate.cshtml.cs
@@ -1,12 +1,20 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DisasterAlleviation.Pages
 {
+    [Authorize("administrator")]
     public class AllocateModel : PageModel
     {
         public void OnGet()
         {
+           
+        }
+        
+        public void OnPost()
+        {
+           
         }
     }
 }
