4ad6aaf62caedb5d9c0f5d052a878b0c0c223c95
Sinelihle Ngqaza <ST10127517@rcconnect.edu.za>
2022-11-24 19:22:53 +0200

    added new feature

--- DisasterAlleviation/Pages/ActiveDisaster.cshtml.cs
+++ DisasterAlleviation/Pages/ActiveDisaster.cshtml.cs
@@ -1,18 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace DisasterAlleviation.Pages
 {
     public class ActiveDisasterModel : PageModel
     {
+        public List<ActiveDisaster> disasterList = new List<ActiveDisaster>();
+        ActiveDisaster activeDisaster = new ActiveDisaster();
+        String ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\IRA\\OneDrive\\Documents\\DisasterDB.mdf;Integrated Security=True;Connect Timeout=30";
+        String error;
         public void OnGet()
         {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConnectionString);
+                con.Open();
+                //retrieving amount from MonetaryDonate table
+                String query = "Select amount from MonetaryDonate";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@amount", activeDisaster.amount);
+                command.ExecuteNonQuery();
+
+                //retrieving good name from GOODS_DONATE table
+                String query2 = "Select goodName from GOODS_DONATE";
+                SqlCommand com = new SqlCommand(query2, con);
+                com.Parameters.AddWithValue("@goodName", activeDisaster.goodsAllocated);
+                com.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            
+
+
         }
     }
 
     public class ActiveDisaster
     {
-        public string disasterName;
         public string amount;
         public string goodsAllocated;
     }
